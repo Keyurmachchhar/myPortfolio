a50427e25c2a0f23c863e750cf8d4c6d721d4a42
Keyurmachchhar <159628049+Keyurmachchhar@users.noreply.github.com>
2024-06-28 16:04:27 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,8 +18,8 @@ const About = () => {
             </motion.div>
             <div className="flex flex-wrap">
                 <motion.div
-                   whileInView={{ opacity: 1, y: 0 }}
-                   initial={{ opacity: 0, y: -100 }}
+                   whileInView={{ opacity: 1, x: 0 }}
+                   initial={{ opacity: 0, x: -100 }}
                    transition={{ duration: 0.5 }}
                     className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
